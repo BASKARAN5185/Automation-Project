80f925a149fbec409ec699c657b3ea59b01faa27
BASKARAN5185 <115864794+BASKARAN5185@users.noreply.github.com>
2024-01-09 23:30:18 -0800

    Update Instagram.java

--- Instagram.java
+++ Instagram.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Instagram {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "D:\\selinum\\chromedriver_106\\chromedriver.exe"); 
+		System.setProperty("webdriver.chrome.driver", "D:\\selinum\\chromedriver_114\\chromedriver.exe"); 
 	    WebDriver driver=new ChromeDriver();
 	    driver.manage().window().maximize();
 	    driver.get("https://www.instagram.com/accounts/login/");
